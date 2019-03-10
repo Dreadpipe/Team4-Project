f3aa5b355b68831a2388176cc40225468b137d1f
Dreadpipe <benharris83@gmail.com>
2019-03-09 21:26:53 -0600

    3-9-19: Created initial files, added jQuery, Firebase, and Bootstrap, and normalize.css links

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Team4-Project
 Team Four Project One!  Let's do the damn thing
+
+Team Four:
+-Joseph S Najera
+-Navreet Singh
+-Benjamin C Harris
+-Toan Takahara
+
+PokeMania (Rev 0)
+
+3/9/16 - Initial commit
+    adding index.html, js/css/assets folders/files
+    adding meta data, bootstrap, jQuery, Firebase, normalize.css
+
+    Ready to go!
---
+++ js/app.js
@@ -0,0 +1,15 @@
+// jQuery active 
+
+
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBQn2tp8-NXzhWu5hcG8kxS6MAGDLkoCNE",
+        authDomain: "pokemania.firebaseapp.com",
+        databaseURL: "https://pokemania.firebaseio.com",
+        projectId: "pokemania",
+        storageBucket: "",
+        messagingSenderId: "959234008264"
+        };
+        
+        firebase.initializeApp(config);
