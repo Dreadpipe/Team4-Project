c372822420cfc840f33b25f8cffaf19df6bc6b32
Dreadpipe <benharris83@gmail.com>
2019-03-17 16:47:46 -0500

    Smashed pages together, smashed js together

--- js/app.js
+++ js/app.js
@@ -4,14 +4,21 @@
 // console.log(pokeData)
     // makes page load first
     $(document).ready(function() {
+        $(".page2").hide();
+        $("body").addClass("page1");
 // on click function, or search result thingy goes here 
 // Currently set up as function to be called for testing purposes
 // Will need to be on-click function that eventually tags to search bar
 $(document).on("click", "#pokemon-input", function(event){
     // prevents page from refreshing
     event.preventDefault();
-    // temporary DIV - this clears the div on every search
-    $("#testDiv").empty();
+    // NEED A "CLEAR" FEATURE
+    $("body").removeClass("page1")
+    $(".page1").hide("drop", {direction: "down"}, "slow");
+    $(".jumbotron").hide();
+    $(".search").hide("drop", {direction: "up"}, "slow");
+    $(".page2").show("fold", 1000);
+    $(".page2").addClass("ui-front")
     // this grabs the user input from the search bar
     const pokemonLower = $("#add-pokemon").val().trim().toLowerCase();
     //  Linking Pokemon API here, calling NAME and SPRITE
@@ -38,6 +45,19 @@ $(document).on("click", "#pokemon-input", function(event){
     });
     // ends on-click event
     });
+// jQuery UI event that resets the board
+$(document).on("click", "#backbtn", function(event) {
+    event.preventDefault();
+    $(".result-name").empty();
+    $(".result").empty();
+    $(".chart").empty();
+    $(".page2").hide("drop", {direction: "left"}, "slow");
+    $("body").addClass("page1");
+    $(".jumbotron").show();
+    $(".search").show("slide", 1000);
+    $(".page1").show("slide", 1000);
+    $(".page2").removeClass("ui-front");
+})
     // ends document ready function
 });
 
